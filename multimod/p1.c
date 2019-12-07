fb7413a18ebe2f834e4c060f5a27d7aa0e815cad
tracer-nju <tracer@nju.edu.cn>
2019-12-07 17:35:35 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: d8129d268a4640879bb6bf3735ced4c8            Boot ID: 65ca5148201746f3b60be0177da7bfac     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  17:35:35 up 2 days,  1:49,  4 users,  load average: 0.23, 0.18, 0.17

--- multimod/p1.c
+++ multimod/p1.c
@@ -2,9 +2,24 @@
 
 int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
   // TODO: implement
-  int64_t multi = ((long double)a)*b/m;
-  int64_t result =a*b-multi*m;
-  while(result<0)result+=m;
-  while(result>=m)result-=m;
+  //int64_t multi = ((long double)a)*b/m;
+  //int64_t result =a*b-multi*m;
+  //while(result<0)result+=m;
+  //while(result>=m)result-=m;
+  int64_t result=0;
+  if(a<b){
+	  for(int i=0;i<a;i++){
+		  int64_t temp = b%m;
+		  result+=temp;
+		  result = result%m;
+	  }
+  }else{
+	  for(int i=0;i<b;i++){
+		  int64_t temp = a%m;
+		  result+=temp;
+		  result = result%m;
+	  }
+  }
+
   return result;
 }
