a7dae5d1c24c414f2167f0995f90f4612b3f7fd1
tracer-nju <tracer@nju.edu.cn>
2019-12-07 16:49:13 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: d8129d268a4640879bb6bf3735ced4c8            Boot ID: 65ca5148201746f3b60be0177da7bfac     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  16:49:13 up 2 days,  1:03,  4 users,  load average: 0.11, 0.12, 0.09

--- multimod/main.c
+++ multimod/main.c
@@ -16,30 +16,30 @@ multimod_t multimod_tab[] = {
 
 int main(int argc, char *argv[]) {
   //int64_t a = 0, b = 0, m = 1;
-  //multimod_t func = NULL;
+  multimod_t func = NULL;
 
-  //while (1) {
-    //int idx;
-    //int c = getopt_long(argc, argv, "i:", long_option, &idx);
-    //if (c == -1) {
-      //break;
-    //} if (c == 'i') {
-      //int i = atoi(optarg);
-      //if (1 <= i && i <= LENGTH(multimod_tab)) {
-        //func = multimod_tab[i - 1];
-      //}
-    //}
-  //}
+  while (1) {
+    int idx;
+    int c = getopt_long(argc, argv, "i:", long_option, &idx);
+    if (c == -1) {
+      break;
+    } if (c == 'i') {
+      int i = atoi(optarg);
+      if (1 <= i && i <= LENGTH(multimod_tab)) {
+        func = multimod_tab[i - 1];
+      }
+    }
+  }
 
-  //if (!func) {
-    //fprintf(stderr, "Usage: lab1 -i 1|2|3 a b m\n");
-    //exit(1);
-  //}
+  if (!func) {
+    fprintf(stderr, "Usage: lab1 -i 1|2|3 a b m\n");
+    exit(1);
+  }
 
   //a = strtoll(argv[optind], NULL, 10);
   //b = strtoll(argv[optind + 1], NULL, 10);
   //m = strtoll(argv[optind + 2], NULL, 10);
 
-  int64_t ret = p1(2, 3, 5);
+  int64_t ret = fuc(2, 3, 5);
   printf("%ld\n", ret);
 }
