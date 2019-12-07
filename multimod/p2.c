7118e42c5fa0457f8fb9cf95eea4b9f91b060817
tracer-nju <tracer@nju.edu.cn>
2019-12-07 20:04:26 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: d8129d268a4640879bb6bf3735ced4c8            Boot ID: 65ca5148201746f3b60be0177da7bfac     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  20:04:26 up 2 days,  3:33,  4 users,  load average: 0.14, 0.11, 0.09

--- multimod/p2.c
+++ multimod/p2.c
@@ -7,7 +7,7 @@ int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
   int64_t result=0;
   while(a){
 	  if(a&1)result = (result+b)%m;
-	  b = (b<<1)%m;
+	  b = (b*2)%m;
 	  a>>=1;
   }
   return result;
