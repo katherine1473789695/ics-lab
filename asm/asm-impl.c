b1bd0b312a554054c9b4aeb655882653fde563a3
tracer-nju <tracer@nju.edu.cn>
2019-12-16 18:35:19 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: d8129d268a4640879bb6bf3735ced4c8            Boot ID: 65ca5148201746f3b60be0177da7bfac     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  18:35:19 up 5 days, 23:27,  5 users,  load average: 0.13, 0.14, 0.13

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -17,7 +17,19 @@ int64_t asm_add(int64_t a, int64_t b) {
 
 int asm_popcnt(uint64_t n) {
   // TODO: implement
-  return 0;
+  uint64_t count = 0;
+  while(n){
+	  asm volatile(
+			  "movq %[n],%%rax;"
+			  "andq $1,%%rax;"
+			  "addq %%rax,%[cnt];"
+			  "shrq %[n];"
+			  :
+			  :[n]"+r"(n),[cnt]"+r"(count)
+			  :"%rax" 
+			  );
+  }
+  return count;
 }
 
 void *asm_memcpy(void *dest, const void *src, size_t n) {
