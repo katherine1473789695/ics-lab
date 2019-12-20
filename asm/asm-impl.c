89dd7f16322cb9d4a32542bbc922ed57b390729f
tracer-nju <tracer@nju.edu.cn>
2019-12-21 01:21:37 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: d8129d268a4640879bb6bf3735ced4c8            Boot ID: 65ca5148201746f3b60be0177da7bfac     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  01:21:37 up 7 days, 12:45,  4 users,  load average: 0.13, 0.11, 0.09

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -82,4 +82,23 @@ int asm_setjmp(asm_jmp_buf env) {
 
 void asm_longjmp(asm_jmp_buf env, int val) {
   // TODO: implement
+  asm volatile(
+		  "movq 120(%[env]),%%r15;"
+		  "movq 112(%[env]),%%r14;"
+		  "movq 104(%[env]),%%r13;"
+		  "movq 96(%[env]),%%r12;"
+		  "movq 88(%[env]),%%r11;"
+		  "movq 80(%[env]),%%r10;"
+		  "movq 72(%[env]),%%r9;"
+		  "movq 64(%[env]),%%r8;"
+		  "movq 56(%[env]),%%rbp;"
+		  "movq 48(%[env]),%%rsp;"
+		  "movq 40(%[env]),%%rdi;"
+		  "movq 32(%[env]),%%rsi;"
+		  "movq 24(%[env]),%%rdx;"
+		  "movq 16(%[env]),%%rcx;"
+		  "movq 8(%[env]),%%rbx;"
+		  :
+		  :[env]"D"(env),[val]"a"(val)
+		  );
 }
