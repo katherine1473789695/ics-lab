30d3a22b7e8018f1daa1d5b4df0a4c3943821d05
tracer-nju <tracer@nju.edu.cn>
2019-12-21 14:21:57 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: d8129d268a4640879bb6bf3735ced4c8            Boot ID: 65ca5148201746f3b60be0177da7bfac     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  14:21:57 up 7 days, 18:59,  1 user,  load average: 0.13, 0.15, 0.11

--- perf/impl.c
+++ perf/impl.c
@@ -18,3 +18,32 @@ void simple_loop() {
   for (volatile int i = 0; i < 1000000; i++) ;
 }
 
+int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
+  // TODO: implement
+  if(a>m)a = a%m;
+  if(b>m)b = b%m;
+  int64_t multi = ((long double)a)*b/m;
+  int64_t result =a*b-multi*m;
+  while(result<0)result+=m;
+  while(result>=m)result-=m;
+
+  return result;
+}
+
+int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
+  // TODO: implement
+  uint64_t na = a%m;
+  uint64_t nb = b%m;
+  uint64_t result=0;
+  while(na){
+	  if(na&1)result = (result+nb)%m;
+	  nb = (nb<<1)%m;
+	  na>>=1;
+  }
+  return result;
+}
+
+int64_t multimod_p3(int64_t a, int64_t b, int64_t m) {
+  int64_t t = (a * b - (int64_t)((double)a * b / m) * m) % m;
+  return t < 0 ? t + m : t;
+}
