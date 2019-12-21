99104929e2bf2d5a165f9bc38d73c56529b4d1f2
tracer-nju <tracer@nju.edu.cn>
2019-12-21 11:29:49 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: d8129d268a4640879bb6bf3735ced4c8            Boot ID: 65ca5148201746f3b60be0177da7bfac     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  11:29:49 up 7 days, 16:07,  7 users,  load average: 0.14, 0.15, 0.10

--- perf/main.c
+++ perf/main.c
@@ -16,7 +16,7 @@ static void (*lookup(const char *fn))();
 
 int main(int argc, char **argv) {
   // TODO: parse arguments: set @func and @rounds
-  printf("%d,%s\n",argc,argv[1]);
+  printf("%d,%s\n",argc,argv[2]);
   void (*func)() = lookup("dummy");
   int rounds = 10;
 
