fab609f653b8c79c6cd92a1bc52c1b25bbca832b
tracer-nju <tracer@nju.edu.cn>
2019-12-22 17:34:01 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: d8129d268a4640879bb6bf3735ced4c8            Boot ID: 65ca5148201746f3b60be0177da7bfac     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  17:34:01 up 8 days,  9:36,  1 user,  load average: 0.19, 0.16, 0.18

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -87,7 +87,10 @@ uint32_t cache_read(uintptr_t addr) {
   if(!flag){
     int random = rand()%caches->line_number;
     //write back the picked line to the mem
-    mem_write((caches->sets[setnum].lines[random].tag<<caches->width_of_setnum)+setnum,caches->sets[setnum].lines[random].data);
+    if(caches->sets[setnum].lines[random].dirty == 1){
+      mem_write((caches->sets[setnum].lines[random].tag<<caches->width_of_setnum)+setnum,caches->sets[setnum].lines[random].data);
+    }
+    
     //read new mem to the picked line
     mem_read(addr>>BLOCK_WIDTH,caches->sets[setnum].lines[random].data);
     //set valid bit and tag
